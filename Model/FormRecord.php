de6fc19ac8aee764e4ba05f494cfc895ce90bf2d
Marcin Frymark <m.frymark@alekseon.com>
2022-04-01 16:41:31 +0200

    102.0.1

--- Model/FormRecord.php
+++ Model/FormRecord.php
@@ -12,23 +12,61 @@ namespace Alekseon\CustomFormsBuilder\Model;
 class FormRecord extends \Alekseon\AlekseonEav\Model\Entity
 {
     /**
+     * @var EmailNotification
+     */
+    protected $emailNotification;
+    /**
+     * @var FormRepository
+     */
+    protected $formRepository;
+
+    /**
      * FormRecord constructor.
      * @param \Alekseon\AlekseonEav\Model\Context $context
      * @param \Magento\Framework\Registry $registry
      * @param ResourceModel\FormRecord $resource
      * @param ResourceModel\FormRecord\Collection $resourceCollection
+     * @param EmailNotification $emailNotification
      */
     public function __construct(
         \Alekseon\AlekseonEav\Model\Context $context,
         \Magento\Framework\Registry $registry,
         \Alekseon\CustomFormsBuilder\Model\ResourceModel\FormRecord $resource,
-        \Alekseon\CustomFormsBuilder\Model\ResourceModel\FormRecord\Collection $resourceCollection
+        \Alekseon\CustomFormsBuilder\Model\ResourceModel\FormRecord\Collection $resourceCollection,
+        \Alekseon\CustomFormsBuilder\Model\EmailNotification $emailNotification,
+        \Alekseon\CustomFormsBuilder\Model\FormRepository $formRepository
     ) {
+        $this->emailNotification  =$emailNotification;
+        $this->formRepository = $formRepository;
         parent::__construct(
             $context,
             $registry,
             $resource,
             $resourceCollection
         );
+    }
+
+    /**
+     * @throws \Magento\Framework\Exception\NoSuchEntityException
+     */
+    public function getForm()
+    {
+        return $this->formRepository->getById($this->getFormId());
+    }
+
+    /**
+     * @param \Magento\Framework\DataObject $object
+     */
+    public function afterSave()
+    {
+        if ($this->isObjectNew() && $this->getForm()->getEnableEmailNotification()) {
+            $this->emailNotification->sendNotificationEmail(
+                [
+                    'form' => $this->getForm(),
+                    'entity' => $this,
+                ]
+            );
+        }
+        parent::afterSave();
     }
 }
---
+++ Setup/UpgradeSchema.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Copyright © Alekseon sp. z o.o.
+ * http://www.alekseon.com/
+ */
+namespace Alekseon\CustomFormsBuilder\Setup;
+
+use Magento\Framework\DB\Ddl\Table;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\SchemaSetupInterface;
+use Magento\Framework\Setup\UpgradeSchemaInterface;
+
+/**
+ * Class UpgradeSchema
+ * @package Alekseon\CustomFormsBuilder\Setup
+ */
+class UpgradeSchema implements UpgradeSchemaInterface
+{
+    /**
+     * @param SchemaSetupInterface $setup
+     * @param ModuleContextInterface $context
+     */
+    public function upgrade(SchemaSetupInterface $setup, ModuleContextInterface $context)
+    {
+        $setup->startSetup();
+        if ($context->getVersion() && version_compare($context->getVersion(), '1.0.1', '<')) {
+            $setup->getConnection()->addColumn(
+                $setup->getTable('alekseon_custom_form_record'),
+                'created_at',
+                [
+                    'type' => Table::TYPE_TIMESTAMP,
+                    'comment' => 'Pickup Time',
+                    'DEFAULT' =>  Table::TIMESTAMP_INIT
+                ]
+            );
+        }
+        $setup->endSetup();
+    }
+}
